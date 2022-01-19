111d1e90cfb81514e202a8230c4e85e073ab66c4
Mert Soyer <mertsoyer@hotmail.com.tr>
2022-01-19 22:44:13 +0300

    türkçe

--- Personal_Expense_Tracker/Startup.cs
+++ Personal_Expense_Tracker/Startup.cs
@@ -28,6 +28,7 @@ namespace Personal_Expense_Tracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+           
             services.AddDbContext<ApplicationDbContext>();
             services.AddDatabaseDeveloperPageExceptionFilter();
 
@@ -42,6 +43,7 @@ namespace Personal_Expense_Tracker
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
